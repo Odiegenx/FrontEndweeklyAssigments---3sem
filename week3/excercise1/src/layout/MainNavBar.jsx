3a5504e109bc51fd856a78c088cdf67224949fff
Odiegenx <124783324+Odiegenx@users.noreply.github.com>
2024-05-11 19:55:49 +0200

    added some styled components

--- week3/excercise1/src/layout/MainNavBar.jsx
+++ week3/excercise1/src/layout/MainNavBar.jsx
@@ -1,33 +1,57 @@
-import { NavLink } from 'react-router-dom'
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 import LogoutButton from "./LogoutButton.jsx";
+
+const NavBar = styled.nav`
+    display: flex;
+    justify-content: space-between;
+    background: aqua;
+`;
+
+const NavList = styled.ul`
+    display: flex;
+    list-style-type: none;
+`;
+
+const NavItem = styled.li`
+    margin-right: 10px;
+`;
+
+const AuthLinks = styled.div`
+    display: flex;
+`;
 
 export default function MainNavBar({currentUser ,setCurrentUser}) {
     return (
-        <nav>
-            <li>
-                <NavLink to="/home">Home</NavLink>
-            </li>
-            <li>
-                <NavLink to="/about">About</NavLink>
-            </li>
-            <li>
-                <NavLink to="/contact">Contact</NavLink>
-            </li>
-            <li>
-                <NavLink to="/posts">Posts</NavLink>
-            </li>
-            {currentUser ?
-                <LogoutButton setCurrentUser={setCurrentUser}/>
-                :
-                <div>
-                    <li>
-                        <NavLink to="/login">Login</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/register">Register</NavLink>
-                    </li>
-                </div>
-            }
-        </nav>
+        <NavBar>
+            <NavList>
+                <NavItem>
+                    <NavLink to="/home">Home</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink to="/about">About</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink to="/contact">Contact</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink to="/posts">Posts</NavLink>
+                </NavItem>
+            </NavList>
+            <NavList>
+                {currentUser ?
+                    <LogoutButton setCurrentUser={setCurrentUser}/>
+                    :
+                    <AuthLinks>
+                        <NavItem>
+                            <NavLink to="/login">Login</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to="/register">Register</NavLink>
+                        </NavItem>
+                    </AuthLinks>
+                }
+            </NavList>
+        </NavBar>
     )
 }
